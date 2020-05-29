4f68dc0190ce5b3fb298d75bb112a9780273b10d
Michael Bade <mbade314@gmail.com>
2020-05-29 19:35:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,13 +13,11 @@ class Scraper
     students = site.css("div.roster-cards-container")
     binding.pry
     students.each do |info|
-      name = info.css("h4.student-name").text
-      location = info.css("p.student-location").text
-      profile_url = info.css("a").attr("href").text
-      hash = {:name => name,
-      :location => location,
-      :profile_url => profile_url
-    }
+      info.each do |student_info|
+        name = student_info.css("h4.student-name").text
+        location = student_info.css("p.student-location").text
+        profile_url = student_info.css("a").attr("href").text
+        hash = {:name => name, :location => location, :profile_url => profile_url}
     all << hash
   end
   all
