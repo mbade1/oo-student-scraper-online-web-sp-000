5dae582e0baacf50c3e9244c446423adcb5f193e
Michael Bade <mbade314@gmail.com>
2020-05-29 19:22:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    binding.pry
+    
     site = Nokogiri::HTML(open(index_url))
 
     all = []
@@ -15,6 +15,7 @@ class Scraper
       name = info.css("h4.student-name").text
       location = info.css("p.student-location").text
       profile_url = info.css("a").attr("href").text
+      binding.pry
       hash = {:name => name,
       :location => location,
       :profile_url => profile_url
