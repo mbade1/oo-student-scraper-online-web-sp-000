705cf4a7ebad8039d5ae5c7f3cbcdfddb94265f7
Michael Bade <mbade314@gmail.com>
2020-05-29 19:49:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,8 @@ class Scraper
     all = []
 
     #binding.pry
-    site.css(".student-card").each do |student_info|
+    students = doc.css(".student-card")
+    students.each do |student_info|
         name = student_info.css("h4.student-name").text
         location = student_info.css("p.student-location").text
         profile_url = student_info.css("a").attr("href").text
