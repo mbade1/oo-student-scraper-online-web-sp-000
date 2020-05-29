2713b08a305ba2f3a27bd872ae05a464ab0e0c91
Michael Bade <mbade314@gmail.com>
2020-05-29 19:06:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,10 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    self.send("name=", all[:name])
+    self.send("location=", all[:location])
+    self.send("profile_url=", all[:profile_url])
+    @@all << self    
   end
 
   def self.create_from_collection(students_array)
