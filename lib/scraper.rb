b2cb52bb8a3216b6b5a4aee99be03992b23fecd0
Michael Bade <mbade314@gmail.com>
2020-05-29 18:32:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,7 +2,8 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
+  html = open('https://learn-co-curriculum.github.io/student-scraper-test-page/index.html')
+  site = Nokogiri::HTML(html)
   def self.scrape_index_page(index_url)
 
   end
